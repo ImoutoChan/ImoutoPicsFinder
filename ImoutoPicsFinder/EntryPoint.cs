df246eb3ecf2aa1c3f2dba86bb46b5c4601f6ec2
ImoutoChan <kirirrin@gmail.com>
2023-05-03 22:56:17 +0400

    Init

---
+++ ImoutoPicsFinder/EntryPoint.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using IqdbApi;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ImoutoPicsFinder;
+
+public static class EntryPoint
+{
+    [FunctionName("ImoutoPicsFinder")]
+    public static async Task Update(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest request,
+        ILogger logger)
+    {
+        logger.LogInformation("PicsFinder function triggered");
+        logger.LogInformation(request.GetDisplayUrl());
+
+        var client = GetTelegramBotClient(logger);
+        var picsFinder = new PicsFinder(client, new IqdbClient());
+
+        var requestBody = await request.ReadAsStringAsync();
+        var update = JsonConvert.DeserializeObject<Update>(requestBody);
+
+        if (update.Message?.Text == "/start")
+        {
+            await client.SendTextMessageAsync(
+                update.Message.Chat.Id,
+                "Send me a picture and I will try to find the source! uwu! 🍑",
+                replyToMessageId: update.Message.MessageId);
+            return;
+        }
+        
+        if (update.Message == null
+            ||update.Type != UpdateType.Message
+            || update.Message?.Photo == null
+            || update.Message?.Photo.Length == 0)
+        {
+            return;
+        }
+
+        try
+        {
+            await picsFinder.ProcessPhotoPostAsync(update.Message!);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "PicsFinder function failed" + e.Message);
+            await client.SendTextMessageAsync(
+                update.Message!.Chat.Id,
+                "Oops! Can't process your picture, please try again uwu 🍑" + "\n" + e.Message,
+                replyToMessageId: update.Message.MessageId);
+        }
+    }
+    
+    private static TelegramBotClient GetTelegramBotClient(ILogger logger)
+    {
+        var token = Environment.GetEnvironmentVariable("TelegramBotToken");
+        if (token is not null) 
+            return new TelegramBotClient(token);
+        
+        logger.LogError(string.Join("|||", Environment.GetEnvironmentVariables()));
+        throw new Exception("Unable to read telegram bot token");
+    }
+}
---
+++ ImoutoPicsFinder/PicsFinder.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IqdbApi;
+using IqdbApi.Enums;
+using IqdbApi.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using MatchType = IqdbApi.Enums.MatchType;
+
+namespace ImoutoPicsFinder;
+
+public class PicsFinder
+{
+    private readonly ITelegramBotClient _telegramBotClient;
+    private readonly IIqdbClient _iqdbClient;
+
+    public PicsFinder(ITelegramBotClient telegramBotClient, IIqdbClient iqdbClient)
+    {
+        _telegramBotClient = telegramBotClient;
+        _iqdbClient = iqdbClient;
+    }
+
+    public async Task ProcessPhotoPostAsync(Message message)
+    {
+        if (message.Photo == null || message.Photo.Length == 0)
+            return;
+
+        var fileId = message.Photo.Last().FileId;
+
+        var replyMessage = await GetReplyMessageAsync(fileId);
+
+        await _telegramBotClient.SendTextMessageAsync(
+            message.Chat.Id, 
+            replyMessage, 
+            ParseMode.Html,
+            replyToMessageId: message.MessageId);
+    }
+
+    private async Task<string> GetReplyMessageAsync(string fileId)
+    {
+        using var ms = new MemoryStream();
+
+        await _telegramBotClient.GetInfoAndDownloadFileAsync(fileId, ms);
+        ms.Seek(0, SeekOrigin.Begin);
+
+        var results = await _iqdbClient.SearchFile(ms);
+
+        return BuildSearchResultMessage(results);
+    }
+
+    private static string BuildSearchResultMessage(SearchResult searchResults)
+    {
+        var sb = new StringBuilder();
+
+        if (!searchResults.IsFound)
+        {
+            sb.AppendLine("🍰 similar images weren't found 😥");
+            return sb.ToString();
+        }
+
+        searchResults
+            .Matches
+            .Where(x => x.MatchType is MatchType.Best or MatchType.Additional)
+            .OrderByDescending(x => x.Similarity / 10)
+            .ThenBy(RankMatch)
+            .ThenByDescending(x => x.Similarity)
+            .ToList()
+            .ForEach(x =>
+                sb.AppendLine(
+                    $"({x.Similarity} 🍑 {x.Resolution.Width}x{x.Resolution.Height} 🍒 {x.Url.Trim('/', '\\')}"));
+
+        return sb.ToString();
+    }
+
+    private static int RankMatch(Match match)
+        => match.Source switch
+        {
+            Source.Yandere => 1,
+            Source.Danbooru => 0,
+            Source.SankakuChannel => 2,
+            Source.Konachan => 3,
+            Source.Gelbooru => 3,
+            _ => 4
+        };
+}
